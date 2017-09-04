fe5b0ceb47febfdf81f347ae00e14391ecd909da
Carmen Stockberger <carmen.stockberger@gmail.com>
2017-09-03 18:17:34 -0700

    added more to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-var correctAnswer;
+$(".videos").hide()
 
 //timer for question & answer(s) page
 function longTimer(){
@@ -12,7 +12,7 @@ function longTimer(){
 		if (count <= 0){
 			clearInterval(counter);
 			console.log("Time's out!")
-			correctPage();
+			timesUp();
 		}
 		console.log(count)
 		$("#timerDisplay").html(count)
@@ -33,36 +33,25 @@ function shortTimer(){
 		}
 	}
 }
+
 //stops timers
 function stop(){
 	clearInterval(intervalId)
 	clockRunning = false;
 }
 
-//function to change to new question...
-function correctPage(){
-	$("#gameDisplay").text("Correct Answer is: " + correctAnswer)
-	shortTimer()
-}
 
-$("#startBtn").on("click", function(){
-	console.log("start this motha")
-	$("#start").empty()
-	longTimer()
-
-	//display text for first question
 	var quizQuestion, i, j, x, correct = "";
 	var quizQuestion = {
 		question: [
 			{quote: "You can't handle the truth!", answers: ["Goodfellas", "Basic Instinct", "A Few Good Men", "Pulp Fiction"], correctAnswer: "A Few Good Men"},
 			{quote: "It's so fluffy I'm gonna die.", answers: ["Despicable Me", "Inside Out", "Wall-E", "Finding Nemo"], correctAnswer: "Despicable Me"},
-			{quote: "-We need this gig. <br> -We're on a mission from God.", answers: ["Wayne's World", "Blues Brothers", "Animal House", "The Big Lebowski"], correctAnswer: "Blues Brothers"},
+			{quote: "We're on a mission from God.", answers: ["Wayne's World", "Blues Brothers", "Animal House", "The Big Lebowski"], correctAnswer: "Blues Brothers"},
 			{quote:"She doesn't even go here", answers: ["Clueless", "Sixteen Candles", "The Princess Diaries", "Mean Girls"], correctAnswer: "Mean Girls"},
 			{quote: "Get busy living or get busy dying", answers: ["Forrest Gump", "The Shawshank Redemption", "The Green Mile", "Fight Club"], correctAnswer: "The Shawshank Redemption"},
 			{quote: "Help me I'm poor", answers: ["Superbad","Meet the Parents","Bridesmaids", "The Hangover"], correctAnswer: "Bridesmaids"},
 			{quote: "In the morning, I'm making waffles", answers: ["Shrek", "Monsters Inc", "Toy Story", "The Incredibles"], correctAnswer: "Shrek"},
-			{quote: "...But what I do have are a very particular set of skills; skills I have aquired over a long career...", answers: ["The Transporter", "Shooter", "Casino Royale","Taken"], correctAnswer: "Taken"}
-
+			//{quote: "Carpe Diem boys. Seize the day.", answers: ["Good Will Hunting", "The Truman Show", "Mrs. Doubtfire","Dead Poets Society"], correctAnswer: "Dead Poets Society"}
 		]
 	}
 		for (i in quizQuestion.question){
@@ -70,20 +59,48 @@ $("#startBtn").on("click", function(){
 			correct += quizQuestion.question[i].correctAnswer;
 			console.log(quizQuestion.question[i].quote)
 			console.log(quizQuestion.question[i].correctAnswer)
+
 			for (j in quizQuestion.question[i].answers){
 				x += quizQuestion.question[i].answers[j];
-				//console.log(quizQuestion.question[i].answers[j])
 			}
 		
 		}
-
-		$("#questionDisplay").text(quizQuestion.question[i].quote)
-	//display answer options as buttons	
+	
 		$("#answer1").val(quizQuestion.question[i].answers[0])
 		$("#answer2").val(quizQuestion.question[i].answers[1])
 		$("#answer3").val(quizQuestion.question[i].answers[2])
 		$("#answer4").val(quizQuestion.question[i].answers[3])
 
+
+//wrong answer display... not working
+function wrongPage(){
+	$("gameDisplay").text("Sorry, wrong answer." + " Correct answer is: " + quizQuestion.question[i].correctAnswer)
+	shortTimer()
+}
+
+//correct answer display		
+function correctPage(){
+	$("#gameDisplay").text("You're right!" + " Correct Answer is: " + quizQuestion.question[i].correctAnswer)
+	shortTimer()
+	$(".videos").show()//video display not working
+}
+
+//time out display... flashes for a second or 2 when timer finishes, then plays correctPage
+function timesUp(){
+	$("#gameDisplay").text("Time is up." + " Correct Answer is: " +quizQuestion.question[i].correctAnswer)
+	shortTimer()
+}
+
+//start button user selects to start the game
+$("#startBtn").on("click", function(){
+	console.log("start this motha")
+	$("#start").empty()
+	longTimer()
+
+//display text for the quiz question to the user
+$("#questionDisplay").text(quizQuestion.question[i].quote)
+
+	//display answer options as buttons	
 		$("#answer1").text(quizQuestion.question[i].answers[0])
 		$("#answer2").text(quizQuestion.question[i].answers[1])
 		$("#answer3").text(quizQuestion.question[i].answers[2])
@@ -91,16 +108,19 @@ $("#startBtn").on("click", function(){
 	
 		$(".answerOption").on("click", function(){
 			console.log(this.value)
-			//var correctAnswer = quizQuestion.question[i].correctAnswer[i]
-			if (this.value===quizQuestion.question[i].correctAnswer[i]){//not working, keeps returning 'undefined'
+
+			if (this.value===quizQuestion.question[i].correctAnswer){
 				console.log("Correct!")
-				//^^^not working, need to find a way to stop timer...
+				$("#questionDisplay").text("You got it right!")
 				correctPage()
 			}
 
-			else {
+			else if (this.value!==quizQuestion.question[i].correctAnswer){
 				console.log("Sorry, nope")
-				correctPage()
+				wrongPage()
+			}
+			else {
+				return
 			}
 		})
 
